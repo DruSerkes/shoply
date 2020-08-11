0e2514c52eff7d5bca768352a790245569e94360
Dru Serkes <DruSerkes@gmail.com>
2020-08-11 18:21:21 -0400

    App renders ProductList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
+import ProductList from './ProductList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">{/* TODO CREATE NAVBAR AND RENDER IT HERE? */}</header>
+			<ProductList />
+		</div>
+	);
 }
 
 export default App;
