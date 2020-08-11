fce3950d444bf5e8138abe1227e998bbd43c4d05
Dru Serkes <DruSerkes@gmail.com>
2020-08-11 18:47:01 -0400

    add ProductCard styles

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './ProductCard.css';
+import upperfirst from 'lodash.upperfirst';
 
 const ProductCard = ({ id, data }) => {
 	return (
-		<div className="ProductCard">
+		<div className="ProductCard" key={id} id={id}>
 			<img src={data.image_url} alt={data.name} />
-			<h4>{data.name}</h4>
+			<h4>{upperfirst(data.name)}</h4>
 			<p>Price: ${data.price}</p>
 			<p>{data.description}</p>
 		</div>
