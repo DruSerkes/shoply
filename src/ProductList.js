6fd4cbb4820d508f8a4cde2e09dcfab27e9c4af1
Dru Serkes <DruSerkes@gmail.com>
2020-08-12 01:59:17 -0400

    add Total component to ProductList

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
 import ProductCard from './ProductCard';
+import Total from './Total';
 import './ProductList.css';
 
 const ProductList = () => {
 	const data = useSelector((state) => state.data, shallowEqual);
+
 	return (
 		<div className="ProductList">
 			<h1>Shoply</h1>
+			<Total products={data.products} />
 			<section className="ProductList-Products">
 				{Object.keys(data.products).map((p) => <ProductCard id={p} data={data.products[p]} />)}
 			</section>
