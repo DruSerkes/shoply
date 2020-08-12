8df1743ad0a5a9cc267e490f7fbfbd206caa185f
Dru Serkes <DruSerkes@gmail.com>
2020-08-12 01:43:38 -0400

    move type property variables to constants

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,17 +1,18 @@
 const INITIAL_STATE = { items: [], totalItems: 0 };
+import { ADD_TO_CART, REMOVE_FROM_CART, CLEAR_CART } from './actionTypes';
 
 export default function cartReducer(state = INITIAL_STATE, action) {
 	switch (action.type) {
-		case 'ADD_TO_CART':
+		case ADD_TO_CART:
 			let items = [ ...state.items, action.id ];
 			let totalItems = items.length;
 			return { ...state, items, totalItems };
-		case 'REMOVE_FROM_CART':
+		case REMOVE_FROM_CART:
 			let idxToRemove = state.items.indexOf(action.id);
 			let updatedItems = state.items.slice(0, idxToRemove).concat(state.items.slice(idxToRemove + 1));
 			let updatedTotalItems = updatedItems.length;
 			return { ...state, items: updatedItems, totalItems: updatedTotalItems };
-		case 'CLEAR_CART':
+		case CLEAR_CART:
 			return { ...state, items: [], totalItems: 0 };
 		default:
 			return state;
