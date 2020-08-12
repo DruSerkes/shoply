60a69af8dc4cee058247e73de3cb5003b2480b6f
Dru Serkes <DruSerkes@gmail.com>
2020-08-12 01:56:23 -0400

    remove commented code

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
 import ProductCard from './ProductCard';
-// import AddRemoveItemButton from './AddRemoveItemButton';
 import './ProductList.css';
 
 const ProductList = () => {
 	const data = useSelector((state) => state.data, shallowEqual);
-	// const cart = useSelector((state) => state.cart, shallowEqual);
 	return (
 		<div className="ProductList">
 			<h1>Shoply</h1>
 			<section className="ProductList-Products">
-				{Object.keys(data.products).map((p) => (
-					<ProductCard id={p} data={data.products[p]} />
-					// {/* <AddRemoveItemButton id={p} items={cart.items} /> */}
-					// {/* </ProductCard> */}
-				))}
+				{Object.keys(data.products).map((p) => <ProductCard id={p} data={data.products[p]} />)}
 			</section>
 		</div>
 	);
