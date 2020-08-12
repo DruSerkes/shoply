65d628641ecf6503aa2229b70e210d41d9578e28
Dru Serkes <DruSerkes@gmail.com>
2020-08-12 01:40:26 -0400

    Add action and reducer unittests

--- src/__tests__/reducers.test.js
+++ src/__tests__/reducers.test.js
@@ -1,1 +1,37 @@
-// TODO add tests for action creators and reducers
+// TODO add tests for reducers
+import { addToCart, removeFromCart, clearCart } from '../reducers/actions';
+import dataReducer from '../reducers/dataReducer';
+import cartReducer from '../reducers/cartReducer';
+
+describe('actions tests', () => {
+	test('add to cart', () => {
+		expect(addToCart(3)).toEqual({ type: 'ADD_TO_CART', id: 3 });
+	});
+	test('remove from cart', () => {
+		expect(removeFromCart(7)).toEqual({ type: 'REMOVE_FROM_CART', id: 7 });
+	});
+	test('clear cart', () => {
+		expect(clearCart()).toEqual({ type: 'CLEAR_CART' });
+	});
+});
+
+describe('reducer tests', () => {
+	test('dataReducer', () => {
+		expect(dataReducer({ testing: true })).toEqual({ testing: true });
+	});
+	test('cart reducer', () => {
+		// expect(cartReducer()).toEqual({ items: [], totalItems: 0 });
+		expect(cartReducer({ items: [], totalItems: 0 }, { type: 'ADD_TO_CART', id: 1 })).toEqual({
+			items      : [ 1 ],
+			totalItems : 1
+		});
+		expect(cartReducer({ items: [ 1 ], totalItems: 1 }, { type: 'REMOVE_FROM_CART', id: 1 })).toEqual({
+			items      : [],
+			totalItems : 0
+		});
+		expect(cartReducer({ items: [ 1 ], totalItems: 1 }, { type: 'CLEAR_CART' })).toEqual({
+			items      : [],
+			totalItems : 0
+		});
+	});
+});
