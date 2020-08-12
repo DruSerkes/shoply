4ff1cd6c9bff34f809965f5a8b709a69733fe4fc
Dru Serkes <DruSerkes@gmail.com>
2020-08-12 14:23:53 -0400

    style ProductDetails. Fix ProductList styles for cards

--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -3,6 +3,7 @@ import { Link, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import AddRemoveItemButton from './AddRemoveItemButton';
 import upperfirst from 'lodash.upperfirst';
+import './ProductDetails.css';
 
 const ProductDetails = () => {
 	const { id } = useParams();
@@ -11,18 +12,24 @@ const ProductDetails = () => {
 	return (
 		<div className="ProductDetails">
 			<h1>{upperfirst(item.name)}</h1>
-			<div className="ProductDetails-Left">
-				<img src={item.image_url} alt={item.name} />
+			<div className="ProductDetails-Container">
+				<div className="ProductDetails-Left">
+					<img src={item.image_url} alt={item.name} />
+					<AddRemoveItemButton id={id} />
+					<small>Add to cart</small>
+				</div>
+				<div className="ProductDetails-Right">
+					<p>
+						<b>Price: </b>${item.price}
+					</p>
+					<h4>Product Details</h4>
+					<p>{item.description}</p>
+					{/* <AddRemoveItemButton id={id} /> */}
+					<Link to="/">
+						<button>Go Back</button>
+					</Link>
+				</div>
 			</div>
-			<div className="ProductDetails-Right">
-				<p>Price: ${item.price}</p>
-				<h4>Details</h4>
-				<p>{item.description}</p>
-				<AddRemoveItemButton id={id} />
-			</div>
-			<Link to="/">
-				<button>Go Back</button>
-			</Link>
 		</div>
 	);
 };
