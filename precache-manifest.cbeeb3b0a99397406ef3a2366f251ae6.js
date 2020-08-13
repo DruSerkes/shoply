8102c36bb4680bd72de29cda2da5d321fdf39ba1
Dru Serkes <DruSerkes@gmail.com>
2020-08-13 01:41:10 -0400

    Updates

---
+++ precache-manifest.cbeeb3b0a99397406ef3a2366f251ae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f91ebd01365de9c88fbd0324bbf86a61",
+    "url": "/shoply/index.html"
+  },
+  {
+    "revision": "2b2d0e0229d17fea2044",
+    "url": "/shoply/static/css/main.de5ed876.chunk.css"
+  },
+  {
+    "revision": "1e2a124746584288b3e2",
+    "url": "/shoply/static/js/2.e31d519a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shoply/static/js/2.e31d519a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b2d0e0229d17fea2044",
+    "url": "/shoply/static/js/main.b844b03e.chunk.js"
+  },
+  {
+    "revision": "a6f247144157d8a8b47c",
+    "url": "/shoply/static/js/runtime-main.4bd548d9.js"
+  }
+]);
